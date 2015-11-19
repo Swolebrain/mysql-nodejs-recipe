a99333a7883653e06428f437640f13e98b1f50ae
Swolebrain <vic574m@gmail.com>
2015-11-18 21:02:04 -0500

    skeleton for collection.html
    skeleton for collection.html

--- server.js
+++ server.js
@@ -16,15 +16,42 @@ connection.connect();
 var port = 9000;
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
 
 //purpose of this is to enable cross domain requests
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	next();
 });
+app.use(    bodyParser.urlencoded( {extended: true})    );
 
 app.get('/transactions', function(req,res){
-	
+	connection.query("SELECT * FROM transactions", function(err, rows){
+        if (!err){
+            res.setHeader("Content-Type", "application/json");
+			res.end(JSON.stringify(rows));//Important! this is how you actually
+                                          //send back a response
+        }else{
+            console.log("There was an eror in the query");
+        }
+    });
+});
+
+app.post('/transactions/:sid/:donation', function(req, res){
+    var sid = req.params.sid;
+    var donation = req.params.donation;
+    connection.query("INSERT INTO transactions (sid, donation) VALUES"+
+                    " ("+sid+", "+donation+")", function(error){
+        if (error){
+            console.log("DB insertion failed: "+error);
+            res.end("DB error");
+        }
+        else{
+            console.log("DB insertion succeeded");
+            res.end("Success");
+        }
+    });
 });
 
 app.listen(port);
